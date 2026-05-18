8a7c4ee19f035873376e669fdf0ac8ddcd97c106
ajay <ajay.wlsdyn@gmail.com>
2026-05-18 23:40:11 +0530

    session-17

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0e0623cb2d0a1a386"
+
+
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instace}]' --query 'Reservations[0].Instances[0].InstanceId' --output text)
+
+    if [$instance != "frontend"]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)    
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+
+    echo "$instance: $IP"
+done
