fa30ebbb22363e725d211abae207f354a3dc9502
ajay <ajay.wlsdyn@gmail.com>
2026-05-18 23:58:52 +0530

    session-17

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ SG_ID="sg-0e0623cb2d0a1a386"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instace}]' --query 'Reservations[0].Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instace}]' --query 'Instances[0].InstanceId' --output text)
 
     if [$instance != "frontend"]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)    
@@ -15,5 +15,5 @@ do
     fi
 
     echo "$instance: $IP"
-    
+
 done
