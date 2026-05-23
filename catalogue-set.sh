3de3a98122123829e9f49fc1965955dae36d890b
ajay <ajay.wlsdyn@gmail.com>
2026-05-23 15:25:18 +0530

    session-19

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -14,7 +14,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-MONGODB_HOST=mongodb.dawshfs.fun
+MONGODB_HOST=mongodb.dawsfs.fun
 
 
 mkdir -p $LOGS_FOLDER
