44037de40c406e2479016d726f4fb73676bf7703
ajay <ajay.wlsdyn@gmail.com>
2026-05-23 14:44:38 +0530

    session-19

--- catalogue.sh
+++ catalogue.sh
@@ -83,14 +83,13 @@ VALIDATE $? "copy mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB Client"
 
-mongosh --host "$MONGODB_HOST" </app/db/master-data.js &>>$LOG_FILE 
-VALIDATE $? "Load catalogue products"
+INDEX=$(mongosh mongodb.dawshfs.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue products"
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
 
 systemctl restart catalogue &>>$LOG_FILE
-VALIDATE $? "Restart SUCCESS"
-
-
-
-
-
-
+VALIDATE $? "Restarted catalogue"
